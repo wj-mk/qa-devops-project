1c1bfce53cb5d95c7caa7c94a0874c71cc89ca30
William Kerley <wjmkerley@gmail.com>
2021-12-13 16:12:11 +0000

    init file updated to start using databases

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "the_key_is_a_secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
-from application import routes
+db = SQLAlchemy(app)
+
+from application import routes, models
