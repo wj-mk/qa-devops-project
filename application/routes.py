b282e021fc6bb0672464a453c26e1cfdf502364b
William Kerley <wjmkerley@gmail.com>
2021-12-13 17:09:06 +0000

    route for updating a record addded

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request
 from application import app, db
-from forms import Exoplanet_Form
+from forms import EditEntry, Exoplanet_Form
 from application.models import Exoplanet
 
+# This route has READ functionality
 @app.route('/')
 @app.route('/index')
 def index():
@@ -10,6 +11,7 @@ def index():
     exoplanets = Exoplanet.query.all()
     return render_template('index.html', title='Home', user=user, exoplanets=exoplanets)
 
+# This route has CREATE functionality
 @app.route('/entry', methods=['GET', 'POST'])
 def entry():
     form = Exoplanet_Form()
@@ -27,3 +29,17 @@ def entry():
             flash(f'New exoplanet data for {form.name.data}')
             return redirect(url_for('index'))
     return render_template('entry.html', title='Enter Exoplanets', form=form)
+
+# This route has UPDATE functionality
+@app.route('/update/<int:id>', methods = ['GET', 'POST'])
+def update(id):
+    form = EditEntry()
+    planet = Exoplanet.query.get(id)
+    if request.method == 'POST' and form.validate_on_submit():
+        planet.name = form.name.data
+        planet.system = form.system.data
+        planet.method = form.method.data
+        planet.year = form.year.data
+        db.session.commit()
+        return(redirect(url_for('index')))
+    return render_template('update.html', title='Update Exoplanets', form=form)
