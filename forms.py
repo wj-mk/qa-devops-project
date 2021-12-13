5b7a5b40d7f1597cf499dc9b427b6ceb6f7500ea
William Kerley <wjmkerley@gmail.com>
2021-12-13 14:32:41 +0000

    created form for entering data

--- forms.py
+++ forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import Stringfield, IntegerField, SubmitField
-from wtforms.fields.simple import StringField
+from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 class Exoplanet_Form(FlaskForm):
-    name = Stringfield('Planet Name', validators=[DataRequired()])
+    name = StringField('Planet Name', validators=[DataRequired()])
     system = StringField('System Name', validators=[DataRequired()])
     method = StringField('Discovery Method', validators=[DataRequired()])
-    year = StringField('Discovery Year', validators=[DataRequired()])
+    year = IntegerField('Discovery Year', validators=[DataRequired()])
     submit = SubmitField("Submit Planet")
