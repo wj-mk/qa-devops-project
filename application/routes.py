9d8c7964ebd79e6aefe1df306e6cfb2a33cfb455
William Kerley <wjmkerley@gmail.com>
2021-12-13 12:31:35 +0000

    updated index route to include test data

--- application/routes.py
+++ application/routes.py
@@ -5,4 +5,19 @@ from application import app
 @app.route('/index')
 def index():
     user = {'username' : 'William'}
-    return render_template('index.html', title='Home', user=user)
+    exoplanets = [
+        {
+            'name': 'AU Mic b',
+            'system': 'AU Mic',
+            'discovery_method': 'Transit',
+            'discover_facility': 'TESS',
+            'confirmed': True,
+            'reference': '',
+            'year': 2020
+        },
+        {
+            'name': 'test1',
+            'system': 'test_system'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, exoplanets=exoplanets)
