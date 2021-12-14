bf3c3b34230b2019e1872343120fc83d2533b7aa
William Kerley <wjmkerley@gmail.com>
2021-12-14 16:10:13 +0000

    errors with db no longer appear to appear

--- flask-app/application/__init__.py
+++ flask-app/application/__init__.py
@@ -4,7 +4,9 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "the_key_is_a_secret"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@mysql:3306/flask-db"
+
+# os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
